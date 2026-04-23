ebb9101a17725f454a1c8d89d3ea23d2bf9c22e0
chiraz-Ag <medjdoubchiraz@gmail.com>
2026-04-23 21:02:54 +0100

    welcome-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
+import { useTheme } from "./hooks/useTheme";
+import "./index.css";
 
 function App() {
+  const { theme, toggle } = useTheme();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route
+          path="/"
+          element={<Welcome theme={theme} toggleTheme={toggle} />}
+        />
       </Routes>
     </BrowserRouter>
   );
